9e07bdb1fe7a2d345e8777e33533a55e55d9ede2
Meaw_zap <118115127+xHexlabx@users.noreply.github.com>
2023-03-11 02:50:24 +0700

    .

---
+++ POSN_2565/Contest1/wheel/SOLUTION/wheel.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n , m , q ; // n <= 100000 , k <= 10 , q <= 100000 mode 1 -> out o(k) mode 2 spin o(100) mode 3 reverse-spin O(100)
+
+int main(){
+
+    ios_base :: sync_with_stdio(0) , cin.tie(0) ;
+
+    cin >> n ;
+
+    return 0 ;
+}
